3938db6c62b8774c231ce32eedb5866afb5b2584
idleberg <git@idleberg.com>
2020-02-04 23:46:36 +0100

    remove unused dependency

--- src/util.js
+++ src/util.js
@@ -1,21 +1,4 @@
-import meta from '../package.json';
-import { install } from 'atom-package-deps';
 import { platform } from 'os';
-
-export function satisfyDependencies() {
-  install(meta.name);
-
-  const packageDeps = meta['package-deps'];
-
-  packageDeps.forEach( packageDep => {
-    if (packageDep) {
-      if (atom.packages.isPackageDisabled(packageDep)) {
-        if (atom.inDevMode()) console.log(`Enabling package '${packageDep}\'`);
-        atom.packages.enablePackage(packageDep);
-      }
-    }
-  });
-}
 
 export function which() {
   return (platform() === 'win32') ? 'where' : 'which';
